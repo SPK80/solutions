73203446c8a3a4d5837c294e5fed4fbbba5a9207
SPK80 <psukonkin@gmail.com>
2022-02-25 19:12:51 +0300

    brackets

--- brackets.js
+++ brackets.js
@@ -1,20 +1,23 @@
 function bracketsGen(cur, open, closed, n) {
 
 	if (cur.length === 2 * n) {
-		if (open == closed)
-			console.log(cur);
+		console.log(cur);
 		return
 	}
 
-	bracketsGen(cur + '(', open + 1, closed, n);
+	if (open < n)
+		bracketsGen(cur + '(', open + 1, closed, n);
 
 	if (closed < open)
 		bracketsGen(cur + ')', open, closed + 1, n);
 
 }
 
-bracketsGen('', 0, 0, 1);
+for (let i = 0;i < 6;i++) {
+	console.log('n=', i);
+	bracketsGen('', 0, 0, i);
 
+}
 	// 0 ''
 	// 1 ()
 
