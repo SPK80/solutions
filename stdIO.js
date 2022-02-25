682b79663f5b2a174257de0bf25e2e0e28eed4b6
SPK80 <psukonkin@gmail.com>
2022-02-25 18:03:36 +0300

    stdIO

---
+++ stdIO.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+
+const readlineInterface = readline.createInterface({ input: process.stdin });
+
+readlineInterface.on('line', line => {
+
+}).on('close', () => {
+
+});
