5a8d80b0ed034fc2ab6159e9779827b2498fa45f
SPK80 <psukonkin@gmail.com>
2022-02-25 17:43:36 +0300

    brackets

--- brackets.js
+++ brackets.js
@@ -1,8 +1,18 @@
-function bracketsGen(brackets, bracket, n) {
-	if (brackets.length >= 2 * n) return;
-	bracketsGen()
+function bracketsGen(cur, open, closed, n) {
+
+	if (cur.length === 2 * n) {
+		if (open == closed)
+			console.log(cur);
+		return
+	}
+	bracketsGen(cur + '(', open + 1, closed, n);
+
+	if (closed < open)
+		bracketsGen(cur + ')', open, closed + 1, n);
+
 }
 
+console.log(bracketsGen('', 0, 0, 1));
 
 	// 0 ''
 	// 1 ()
